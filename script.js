bdf4bdaede19c3925f03ad14ef931f4c344d6e60
Serxver <138546291+Serxver@users.noreply.github.com>
2023-07-08 20:09:15 -0500

    Add files via upload

---
+++ script.js
@@ -0,0 +1,9 @@
+function showMessage() {
+    alert("$5.00");
+  }
+  function showtheMessage() {
+    alert("$5.00");
+  }
+  function showtheeMessage() {
+    window.location.href = 'http://bash-1009811.square.site/';
+  }
