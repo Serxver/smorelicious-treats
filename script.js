3facf60c0cb7f2cc8d42adb1eeefcdf74c5d7b81
Serxver <138546291+Serxver@users.noreply.github.com>
2023-07-16 15:56:06 -0500

    Update script.js

--- script.js
+++ script.js
@@ -5,5 +5,5 @@ function showMessage() {
     alert("$5.00");
   }
   function showtheeMessage() {
-    window.location.href = 'http://bash-1009811.square.site/';
+    window.location.href = 'https://view.forms.app/noahchandler/custom-cake-order-form';
   }
